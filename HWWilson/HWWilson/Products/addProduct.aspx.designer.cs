d8b6c67d42d263564c425a1c5294aa73782b69dd
LisaBunnyB <lbunclark@blueyonder.co.uk>
2015-01-29 14:20:41 +0000

    addProduct form validation

--- HWWilson/HWWilson/Products/addProduct.aspx.designer.cs
+++ HWWilson/HWWilson/Products/addProduct.aspx.designer.cs
@@ -31,6 +31,15 @@ namespace HWWilson {
         protected global::System.Web.UI.WebControls.TextBox TextProdName;
         
         /// <summary>
+        /// RequiredFieldValidator1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RequiredFieldValidator RequiredFieldValidator1;
+        
+        /// <summary>
         /// LblProdBarcode control.
         /// </summary>
         /// <remarks>
@@ -47,6 +56,15 @@ namespace HWWilson {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.TextBox TextProdBarcode;
+        
+        /// <summary>
+        /// RequiredFieldValidator2 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RequiredFieldValidator RequiredFieldValidator2;
         
         /// <summary>
         /// LblProdMinLevel control.
@@ -67,6 +85,15 @@ namespace HWWilson {
         protected global::System.Web.UI.WebControls.TextBox TextProdMinLevel;
         
         /// <summary>
+        /// RequiredFieldValidator3 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RequiredFieldValidator RequiredFieldValidator3;
+        
+        /// <summary>
         /// LblProdStockLevel control.
         /// </summary>
         /// <remarks>
@@ -83,6 +110,15 @@ namespace HWWilson {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.TextBox TextProdStockLevel;
+        
+        /// <summary>
+        /// RequiredFieldValidator4 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RequiredFieldValidator RequiredFieldValidator4;
         
         /// <summary>
         /// LblProdStockCode control.
@@ -103,6 +139,15 @@ namespace HWWilson {
         protected global::System.Web.UI.WebControls.TextBox TextProdStockCode;
         
         /// <summary>
+        /// RequiredFieldValidator5 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RequiredFieldValidator RequiredFieldValidator5;
+        
+        /// <summary>
         /// DDLProdCat control.
         /// </summary>
         /// <remarks>
@@ -119,6 +164,15 @@ namespace HWWilson {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.SqlDataSource SqlDataSource1;
+        
+        /// <summary>
+        /// RequiredFieldValidator6 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RequiredFieldValidator RequiredFieldValidator6;
         
         /// <summary>
         /// ProdSubmit control.
