7124fd944df0e3c15994d5bfa961dfea1fbd122e
LisaBunnyB <lbunclark@blueyonder.co.uk>
2015-02-03 12:37:56 +0000

    Login page created

---
+++ HWWilson/Login.aspx
@@ -0,0 +1,9 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Login.aspx.cs" Inherits="HWWilson.Login" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+    <asp:Label ID="LblUserName" runat="server" Text="Label">UserName</asp:Label>
+    <asp:TextBox ID="TxtUserName" runat="server">Trade</asp:TextBox><br />
+      <asp:Label ID="LblPass" runat="server" Text="Label">Password</asp:Label>
+    <asp:TextBox ID="TxtPass" runat="server">Trade</asp:TextBox><br />
+    <asp:Button ID="BtnLogin" onclick="BtnLogin_Click"  runat="server" Text="Login" />
+    <asp:Label ID="Label1" runat="server" Text=""></asp:Label>
+</asp:Content>
---
+++ HWWilson/Login.aspx.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.Security;
+using HWWilson.App_Code;
+
+
+namespace HWWilson
+{
+    public partial class Login : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void BtnLogin_Click(object sender, EventArgs e)
+        {
+            string roles;
+            string username = TxtUserName.Text.Trim();
+            if (DBHelper.CheckUser(username, TxtPass.Text.Trim()) == true)
+            {
+                //These session values are just for demo purpose to show the user details on master page
+                Session["User"] = username;
+                roles = DBHelper.GetUserRoles(username);
+                Session["Roles"] = roles;
+
+                //Let us now set the authentication cookie so that we can use that later.
+                FormsAuthentication.SetAuthCookie(username, false);
+
+                //Login successful lets put him to requested page
+                string returnUrl = Request.QueryString["ReturnUrl"] as string;
+
+                if (returnUrl != null)
+                {
+                    Response.Redirect(returnUrl);
+                }
+                else
+                {
+                    //no return URL specified so lets kick him to home page
+                    Response.Redirect("Default.aspx");
+                }
+            }
+            else
+            {
+                Label1.Text = "Login Failed";
+            }
+
+        }
+    }
+}
