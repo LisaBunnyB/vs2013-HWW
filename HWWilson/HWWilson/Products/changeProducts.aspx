b37d9ad2bc632714be7015b7d57394ec41983542
LisaBunnyB <lbunclark@blueyonder.co.uk>
2015-04-07 17:37:42 +0100

    Edit of products on changeProducts page working, need to add DDL for cat

--- HWWilson/HWWilson/Products/changeProducts.aspx
+++ HWWilson/HWWilson/Products/changeProducts.aspx
@@ -1,4 +1,73 @@
-﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="changeProducts.aspx.cs" Inherits="HWWilson.HWWilson.Products.changeProducts" %>
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="changeProducts.aspx.cs" Inherits="HWWilson.changeProducts" %>
+
 <asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
-    <h1>Change product details</h1>
+    <h1><span><%:Session["name"] %></span>- Change the details of a product</h1>
+
+    <asp:GridView ID="GVEditProducts" runat="server" AutoGenerateColumns="False" 
+        AllowSorting="True" 
+        DataKeyNames="product_id"
+        onrowcancelingedit="GVEditProducts_RowCancelingEdit"
+        onrowediting="GVEditProducts_RowEditing"
+        onrowupdating="GVEditProducts_RowUpdating">
+        <Columns>
+            <asp:CommandField ButtonType="Button" ShowEditButton="true" ShowCancelButton="true" />
+
+            <asp:BoundField DataField="product_id" HeaderText="Product ID" ReadOnly="true" />
+            <asp:TemplateField HeaderText="Barcode">
+                <ItemTemplate>
+                    <%# Eval("barcode")%>
+                </ItemTemplate>
+                <EditItemTemplate>
+                    <asp:TextBox runat="server" ID="txtProductBarcode" Text='<%# Eval("barcode")%>' />
+                </EditItemTemplate>
+            </asp:TemplateField>
+
+            <asp:TemplateField HeaderText="Product Description">
+                <ItemTemplate>
+                    <%# Eval("product_name")%>
+                </ItemTemplate>
+                <EditItemTemplate>
+                    <asp:TextBox runat="server" ID="txtproduct_name" Text='<%# Eval("product_name")%>' />
+                </EditItemTemplate>
+            </asp:TemplateField>
+             <asp:TemplateField HeaderText="Min Level">
+                <ItemTemplate>
+                    <%# Eval("product_min_level")%>
+                </ItemTemplate>
+                <EditItemTemplate>
+                    <asp:TextBox runat="server" ID="txtproduct_min_level" Text='<%# Eval("product_min_level")%>' />
+                </EditItemTemplate>
+            </asp:TemplateField>
+            <asp:TemplateField HeaderText="In Stock">
+                <ItemTemplate>
+                    <%# Eval("prod_stock_level")%>
+                </ItemTemplate>
+                <EditItemTemplate>
+                    <asp:TextBox runat="server" ID="txtprod_stock_level" Text='<%# Eval("prod_stock_level")%>' />
+                </EditItemTemplate>
+            </asp:TemplateField>
+             <asp:TemplateField HeaderText="Stock Code">
+                <ItemTemplate>
+                    <%# Eval("prod_stock_code")%>
+                </ItemTemplate>
+                <EditItemTemplate>
+                    <asp:TextBox runat="server" ID="txtprod_stock_code" Text='<%# Eval("prod_stock_code")%>' />
+                </EditItemTemplate>
+            </asp:TemplateField>
+            <asp:TemplateField HeaderText="Product Category">
+                <ItemTemplate>
+                    <%# Eval("prod_cat_desc")%>
+                </ItemTemplate>
+                <EditItemTemplate>
+                    <asp:TextBox runat="server" ID="txtprod_cat_desc" Text='<%# Eval("prod_cat_desc")%>' />
+                </EditItemTemplate>
+            </asp:TemplateField>
+           
+        </Columns>
+    </asp:GridView>
+
+
+    <asp:TextBox ID="TxtAmendConf" runat="server"></asp:TextBox>
+
+
 </asp:Content>
--- HWWilson/HWWilson/Products/changeProducts.aspx.cs
+++ HWWilson/HWWilson/Products/changeProducts.aspx.cs
@@ -1,17 +1,95 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Collections;
 using System.Web;
+using System.Web.Security;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+using System.Web.UI.HtmlControls;
+using HWWilson.App_Code;
 
-namespace HWWilson.HWWilson.Products
+namespace HWWilson
 {
     public partial class changeProducts : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (!IsPostBack)
+            {
 
+                BindDataGV();
+            }
+        }// closes the Page_Load
+
+        private void BindDataGV()
+        { // populates a gridview with all categories
+            Products myProds = new Products();
+            SqlDataReader myDataReader = myProds.GetProduct();
+            GVEditProducts.DataSource = myDataReader;
+            GVEditProducts.DataBind();
+        }//Closes fillEditProds
+
+        protected void GVEditProducts_RowCancelingEdit(object sender, GridViewCancelEditEventArgs e)
+        {
+            e.Cancel = true;
+            GVEditProducts.EditIndex = -1;
+            BindDataGV();
         }
-    }
-}
+
+        protected void GVEditProducts_RowEditing(object sender, GridViewEditEventArgs e)
+        {
+            GVEditProducts.EditIndex = e.NewEditIndex;
+            BindDataGV();   
+        }
+
+        protected void GVEditProducts_RowUpdating(object sender, GridViewUpdateEventArgs e)
+        {
+            GridViewRow row = GVEditProducts.Rows[e.RowIndex];
+            TextBox txtProductBarcode = (TextBox)row.FindControl("txtProductBarcode");
+            TextBox txtproduct_name = (TextBox)row.FindControl("txtproduct_name");
+            TextBox txtproduct_min_level = (TextBox)row.FindControl("txtproduct_min_level");
+            TextBox txtprod_stock_level = (TextBox)row.FindControl("txtprod_stock_level");
+            TextBox txtprod_stock_code = (TextBox)row.FindControl("txtprod_stock_code");
+            TextBox txtprod_cat_desc = (TextBox)row.FindControl("tprod_cat_desc");
+
+            Products changeProd = new Products();
+            changeProd.prodID = Convert.ToInt16(GVEditProducts.DataKeys[e.RowIndex].Value.ToString());
+            changeProd.prodBar = Convert.ToInt64(txtProductBarcode.Text);
+            changeProd.productName = Convert.ToString(txtproduct_name.Text);
+            changeProd.prodMinLevel = Convert.ToInt32(txtproduct_min_level.Text);
+            changeProd.prodStockLevel = Convert.ToInt32(txtprod_stock_level.Text);
+            changeProd.prodStockCode = Convert.ToString(txtprod_stock_code.Text);
+            changeProd.AmendProduct();
+            String errNumber = Convert.ToString(changeProd.errNo);
+
+            if (changeProd.errNo.Equals(2627) && (changeProd.errMsg.Contains("Error in Adding barcode")))
+            {
+                TxtAmendConf.Text = "The barcode already exists in the database";
+                          
+            }
+            else if (changeProd.errNo.Equals(2627) && (changeProd.errMsg.Contains("Error in Adding product")))
+            {
+                TxtAmendConf.Text = "The stock code already exists in the database";
+                
+            }
+            else
+            {
+                //displays a confirmation that the product has been added to the database
+                TxtAmendConf.Text = changeProd.errNo + "The product has been successfully added to the database";
+                
+            }
+            updategrid();
+
+           
+        }
+
+        private void updategrid()
+        {
+            GVEditProducts.EditIndex = -1;
+            BindDataGV();
+        }
+    }// Closes changeProducts
+}// Closes the namespace
